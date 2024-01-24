dc96ba6746fb0daec4196512954229cc309c9191
Kazaxthan <carlo.paule.29@gmail.com>
2024-01-25 04:22:10 +0800

    Second Commit, here I wrote a code where the base is 5 and 4 is the exponent, it will find its product and print it.

--- exercise15.py
+++ exercise15.py
@@ -1,2 +1,12 @@
 #Exercise 15: Write a function called exponent(base, exp) that returns an int value of base raises to the power of exp.
 #Note here exp is a non-negative integer, and the base is an integer.
+
+base = 5
+exponent = 4
+
+result = base ** exponent
+
+print(f"The result of {base} raised to the power of {exponent} is: {result}")
+
+
+
